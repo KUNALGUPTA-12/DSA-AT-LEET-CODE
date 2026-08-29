b6b35be60e16213e15da9ee378c8b37aa5ee010e
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-08-29 11:22:19 +0530

    Time: 1 ms (99.94%) | Memory: 77.2 MB (48.96%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,15 +1,39 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
+        // kadens patternbs se 
+
+
+        // int maxSum = nums[0];
+        // int currentSum = 0;
+        // int n = nums.length;
+        // for(int i = 0;i < n;i++){
+        //     currentSum += nums[i];
+        //     if(currentSum > maxSum){
+        //         maxSum = currentSum;
+        //     }
+        //     if(currentSum < 0){
+        //         currentSum = 0;
+        //     }
+        // }
+        // return maxSum;/
+        // sliding window se
+
+        int maxSum = nums[0]; // Pehle element se start karenge
         int currentSum = 0;
-        int n = nums.length;
-        for(int i = 0;i < n;i++){
-            currentSum += nums[i];
-            if(currentSum > maxSum){
-                maxSum = currentSum;
+        
+        int left = 0; // Window ka starting point
+        
+        for (int right = 0; right < nums.length; right++) {
+            currentSum += nums[right]; // Window ko right se badao
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum; // Naya bada sum mila toh save karo
             }
-            if(currentSum < 0){
-                currentSum = 0;
+            
+            // Dynamic Window Rule: Agar sum negative ho jaye, toh purani window kharab hai
+            if (currentSum < 0) {
+                currentSum = 0;        // Sum zero karo
+                left = right + 1;      // Window ko shrink karke naye element par le jao
             }
         }
         return maxSum;
