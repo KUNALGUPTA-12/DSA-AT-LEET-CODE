676f097c0da3274ab228360e63986bc66640af8a
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-08-31 10:17:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (87.26%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if(nums.empty()) return 0;
+
+        int max_pro = nums[0],min_pro = nums[0],n = nums.size(),ans = nums[0];
+
+        for(size_t i = 1;i < n;i++){
+            // agar elment negative hai toh
+            if(nums[i] < 0){
+                swap(max_pro, min_pro);
+            }
+            max_pro = max(nums[i], max_pro*nums[i]);
+            min_pro = min(nums[i], min_pro*nums[i]);
+
+            ans = max(ans, max_pro);
+        }
+        return ans;
+        // if (nums.empty()) return 0;
+
+        // int max_prod = nums[0];
+        // int min_prod = nums[0];
+        // int ans = nums[0];
+
+        // for (size_t i = 1; i < nums.size(); i++) {
+        //     // Agar negative number hai toh swap identity
+        //     if (nums[i] < 0) {
+        //         swap(max_prod, min_prod);
+        //     }
+
+        //     max_prod = max(nums[i], max_prod * nums[i]);
+        //     min_prod = min(nums[i], min_prod * nums[i]);
+
+        //     ans = max(ans, max_prod);
+        // }
+
+        // return ans;
+    }
+};
