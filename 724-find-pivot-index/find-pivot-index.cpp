56d2ef39ac207db37f65466c11db455939adbe3e
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-01 11:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (29.49%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        // int total_sum = 0,left_sum = 0;
-        // for(int num : nums){
-        //     total_sum += num;
-        // }
-
-        // for(int num : nums){
-        //     total_sum -= num;
-
-        //     if(left_sum == total_sum){
-        //         return num;
-        //     }
-        //     left_sum += num;
-        // }
-        // return -1;
-        // simple loop wala
-        int total_sum = 0;
-        int left_sum = 0;
-        int n = nums.size();
-        
-        // Step 1: Poore array ka total sum nikal lo (Right Side tarazu)
-        for (int i = 0; i < n; i++) {
-            total_sum += nums[i];
+        int total_sum = 0,left_sum = 0;
+        for(int num : nums){
+            total_sum += num;
         }
-        
-        // Step 2: Har ek index par jaakar check karo
-        for (int i = 0; i < n; i++) {
-            // Right side se current element ko minus karo
-            total_sum -= nums[i]; 
-            
-            // Agar left aur right side ka sum barabar ho gaya
-            if (left_sum == total_sum) {
-                return i; // Mil gaya pivot index!
+        int i = 0;
+        for(int num : nums){
+            total_sum -= num;
+
+            if(left_sum == total_sum){
+                return i;
             }
-            
-            // Agar barabar nahi hai, toh ise left side mein plus kar do
-            left_sum += nums[i];
+            left_sum += num;
+            i++;
         }
+        return -1;
+
+
+
+        // simple loop wala
+        // int total_sum = 0;
+        // int left_sum = 0;
+        // int n = nums.size();
         
-        // Agar poore loop mein kahin nahi mila toh -1 return karo
-        return -1; 
+        // // Step 1: Poore array ka total sum nikal lo (Right Side tarazu)
+        // for (int i = 0; i < n; i++) {
+        //     total_sum += nums[i];
+        // }
+        
+        // // Step 2: Har ek index par jaakar check karo
+        // for (int i = 0; i < n; i++) {
+        //     // Right side se current element ko minus karo
+        //     total_sum -= nums[i]; 
+            
+        //     // Agar left aur right side ka sum barabar ho gaya
+        //     if (left_sum == total_sum) {
+        //         return i; // Mil gaya pivot index!
+        //     }
+            
+        //     // Agar barabar nahi hai, toh ise left side mein plus kar do
+        //     left_sum += nums[i];
+        // }
+        
+        // // Agar poore loop mein kahin nahi mila toh -1 return karo
+        // return -1; 
     }
 };
