5d08df094f9be5298e4478a3ee3b79ccec3b1f89
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-06 16:18:33 +0530

    Time: 10 ms (30.84%) | Memory: 13.4 MB (91.20%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -4,18 +4,33 @@ public:
         // "Back" ka matlab hota hai "Aakhiri Chhor" (End of the string).
         
         // yah stack ki madad se hua hai 
-        string ans = "";//yeh humara khali stack hai
+        // string ans = "";//yeh humara khali stack hai
 
-        for(int i = 0 ;i < s.length();i++){
-            char c = s[i];
-        // for(char c : s){
-            if(!ans.empty() && ans.back() == c){
-                ans.pop_back();// du[licate ko delete karo
+        // // simple and a traditonal loop
+        // for(int i = 0 ;i < s.length();i++){
+        //     char c = s[i];
+        // // for(char c : s){
+        //     if(!ans.empty() && ans.back() == c){
+        //         ans.pop_back();// du[licate ko delete karo
+        //     }
+        //     else{
+        //         ans.push_back(c);//c mai jo hai vo stack mai dal do
+        //     }
+        // }
+        // return ans;
+
+        // isko two pointers approach
+        int i = -1;
+        for(int j = 0;j < s.length();j++){
+            // agar i valid index par hai picha likha hua hai 
+            if(i >= 0 && s[i]==s[j]){
+                i--;
             }
             else{
-                ans.push_back(c);//c mai jo hai vo stack mai dal do
+                i++;
+                s[i] = s[j];
             }
         }
-        return ans;
+        return s.substr(0,i+1);
     }
 };
