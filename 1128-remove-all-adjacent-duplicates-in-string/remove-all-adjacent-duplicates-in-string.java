b832d4a1f167d22853b096f2325b349631a71b5c
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-06 16:20:34 +0530

    Time: 18 ms (72.69%) | Memory: 46.2 MB (96.71%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String removeDuplicates(String s) {
+         StringBuilder ans = new StringBuilder(); // StringBuilder as Stack
+        
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            int len = ans.length();
+            
+            // Agar stack khali nahi hai aur aakhiri character same hai
+            if (len > 0 && ans.charAt(len - 1) == c) {
+                ans.deleteCharAt(len - 1); // Top element ko remove kar do
+            } else {
+                ans.append(c); // Stack mein add kar do
+            }
+        }
+        return ans.toString();
+
+        // two pointers approach
+        // char[] arr = s.toCharArray(); // In-place badlav ke liye array
+        // int i = -1; // Write pointer
+        
+        // for (int j = 0; j < arr.length; j++) {
+        //     // Agar i valid hai aur pichla likha hua arr[i] naye arr[j] ke barabar hai
+        //     if (i >= 0 && arr[i] == arr[j]) {
+        //         i--; // Duplicate mila, toh write pointer peeche hato
+        //     } else {
+        //         i++; // Agle ghar mein jao
+        //         arr[i] = arr[j]; // Character overwrite karo
+        //     }
+        // }
+        // // Array se 0 se leke i+1 length tak ki filtered string banakar return karo
+        // return new String(arr, 0, i + 1);
+    }
+}
