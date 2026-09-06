892bebeaab5371df3b70b68249cc22cd7eb25d72
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-06 17:10:46 +0530

    Time: 1 ms (99.94%) | Memory: 42.7 MB (98.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -33,65 +33,65 @@ class Solution {
         // //     return false; // Stack khali nahi hai -> Invalid ❌
         // // }
 
-        // string builder se 
-        StringBuilder ans = new StringBuilder(); // StringBuilder ko stack banaya
+        // // string builder se 
+        // StringBuilder ans = new StringBuilder(); // StringBuilder ko stack banaya
         
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                ans.append(c); // Element ko end mein add kiya (Push)
-            } else {
-                int len = ans.length();
-                // Close bracket aaya par StringBuilder khali hai -> Invalid!
-                if (len == 0) return false;
-                
-                // ans.charAt(len - 1) se sabse aakhiri (top) element ko check kiya
-                if ((c == ')' && ans.charAt(len - 1) == '(') || 
-                    (c == '}' && ans.charAt(len - 1) == '{') || 
-                    (c == ']' && ans.charAt(len - 1) == '[')) {
-                    ans.deleteCharAt(len - 1); // Jodidar mila toh end se hata do (Pop)
-                } else {
-                    return false;
-                }
-            }
-        }
-        
-        return ans.isEmpty();
-        
-        // // Simple if-else check length ke basis par
-        // if (ans.length() == 0) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-
-        // two pointers se 
-        //  char[] arr = s.toCharArray(); // In-place change karne ke liye array banaya
-        // int i = -1; // Write pointer (stack top ki tarah)
-        
-        // for (char c : arr) {
+        // for (char c : s.toCharArray()) {
         //     if (c == '(' || c == '{' || c == '[') {
-        //         i++;
-        //         arr[i] = c; // Open bracket ko array mein write kar diya
+        //         ans.append(c); // Element ko end mein add kiya (Push)
         //     } else {
-        //         // Close bracket aaya par VIP line khali hai (i < 0) -> Invalid!
-        //         if (i < 0) return false;
+        //         int len = ans.length();
+        //         // Close bracket aaya par StringBuilder khali hai -> Invalid!
+        //         if (len == 0) return false;
                 
-        //         // arr[i] par baitha hai hamara aakhiri open bracket
-        //         if ((c == ')' && arr[i] == '(') || 
-        //             (c == '}' && arr[i] == '{') || 
-        //             (c == ']' && arr[i] == '[')) {
-        //             i--; // Match ho gaya, pointer peeche le jao (Pop)
+        //         // ans.charAt(len - 1) se sabse aakhiri (top) element ko check kiya
+        //         if ((c == ')' && ans.charAt(len - 1) == '(') || 
+        //             (c == '}' && ans.charAt(len - 1) == '{') || 
+        //             (c == ']' && ans.charAt(len - 1) == '[')) {
+        //             ans.deleteCharAt(len - 1); // Jodidar mila toh end se hata do (Pop)
         //         } else {
         //             return false;
         //         }
         //     }
         // }
         
-        // // i wapas -1 par aa gaya matlab saare brackets clear ho gaye (Empty)
-        // if (i == -1) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
+        // return ans.isEmpty();
+        
+        // // // Simple if-else check length ke basis par
+        // // if (ans.length() == 0) {
+        // //     return true;
+        // // } else {
+        // //     return false;
+        // // }
+
+        // two pointers se 
+         char[] arr = s.toCharArray(); // In-place change karne ke liye array banaya
+        int i = -1; // Write pointer (stack top ki tarah)
+        
+        for (char c : arr) {
+            if (c == '(' || c == '{' || c == '[') {
+                i++;
+                arr[i] = c; // Open bracket ko array mein write kar diya
+            } else {
+                // Close bracket aaya par VIP line khali hai (i < 0) -> Invalid!
+                if (i < 0) return false;
+                
+                // arr[i] par baitha hai hamara aakhiri open bracket
+                if ((c == ')' && arr[i] == '(') || 
+                    (c == '}' && arr[i] == '{') || 
+                    (c == ']' && arr[i] == '[')) {
+                    i--; // Match ho gaya, pointer peeche le jao (Pop)
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        // i wapas -1 par aa gaya matlab saare brackets clear ho gaye (Empty)
+        if (i == -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
