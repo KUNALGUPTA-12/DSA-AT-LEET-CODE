37cf0f47142359c794f8445b3d3dbb57fba6181e
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-07 12:27:15 +0530

    Time: 16 ms (19.57%) | Memory: 46.9 MB (34.62%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,22 +1,46 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // vector wala hai 
-        int[] magazineFreq = new int[128]; // Fixed size java array
+        // // vector wala hai 
+        // int[] magazineFreq = new int[128]; // Fixed size java array
         
-        // Loop 1: Magazine inventory update
+        // // Loop 1: Magazine inventory update
+        // for (int i = 0; i < magazine.length(); i++) {
+        //     char magChar = magazine.charAt(i);
+        //     magazineFreq[magChar]++;
+        // }
+        
+        // // Loop 2: Ransom Note verification from magazineFreq array
+        // for (int i = 0; i < ransomNote.length(); i++) {
+        //     char noteChar = ransomNote.charAt(i);
+            
+        //     if (magazineFreq[noteChar] == 0) {
+        //         return false;
+        //     }
+        //     magazineFreq[noteChar]--;
+        // }
+        
+        // return true;
+
+        // hashmap se hua hai
+
+        HashMap<Character, Integer> magazineFreq = new HashMap<>(); // Array hata kar map banaya
+        
+        // Loop 1: On-the-fly key-value insertion
         for (int i = 0; i < magazine.length(); i++) {
             char magChar = magazine.charAt(i);
-            magazineFreq[magChar]++;
+            magazineFreq.put(magChar, magazineFreq.getOrDefault(magChar, 0) + 1);
         }
         
-        // Loop 2: Ransom Note verification from magazineFreq array
+        // Loop 2: Checking using map methods
         for (int i = 0; i < ransomNote.length(); i++) {
             char noteChar = ransomNote.charAt(i);
             
-            if (magazineFreq[noteChar] == 0) {
+            // Map se value nikali (agar key nahi mili toh default 0 milega)
+            if (magazineFreq.getOrDefault(noteChar, 0) == 0) {
                 return false;
             }
-            magazineFreq[noteChar]--;
+            // Stock mein se ek piece kam kiya
+            magazineFreq.put(noteChar, magazineFreq.get(noteChar) - 1);
         }
         
         return true;
