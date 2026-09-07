f34500bbdac0506d4143b4c83a1a478591fef8a6
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-07 10:58:59 +0530

    Time: 33 ms (20.70%) | Memory: 46.6 MB (92.97%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -20,41 +20,41 @@ class Solution {
         // freq wala 
 
         // Array ka naam 'freq' rakha, Java automatic sabko 0 kar dega
-        int[] freq = new int[128]; 
+        // int[] freq = new int[128]; 
         
-        // Pass 1: Frequency count karna
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i)]++; 
-        }
-        
-        // Pass 2: Pehla unique character dhundna
-        for (int i = 0; i < s.length(); i++) {
-            if (freq[s.charAt(i)] == 1) {
-                return i; 
-            }
-        }
-        
-        return -1;
-
-        // // hashmap wala 
-
-        //  // Asli HashMap structure (shuruat mein bilkul khali)
-        // HashMap<Character, Integer> freq = new HashMap<>();
-        
-        // // Pass 1: Frequency count update karna
+        // // Pass 1: Frequency count karna
         // for (int i = 0; i < s.length(); i++) {
-        //     char c = s.charAt(i);
-        //     // Agar c pehle se hai toh count + 1, nahi toh 0 + 1 (Initialization on the fly)
-        //     freq.put(c, freq.getOrDefault(c, 0) + 1);
+        //     freq[s.charAt(i)]++; 
         // }
         
         // // Pass 2: Pehla unique character dhundna
         // for (int i = 0; i < s.length(); i++) {
-        //     if (freq.get(s.charAt(i)) == 1) {
+        //     if (freq[s.charAt(i)] == 1) {
         //         return i; 
         //     }
         // }
         
         // return -1;
+
+        // hashmap wala 
+
+         // Asli HashMap structure (shuruat mein bilkul khali)
+        HashMap<Character, Integer> freq = new HashMap<>();
+        
+        // Pass 1: Frequency count update karna
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // Agar c pehle se hai toh count + 1, nahi toh 0 + 1 (Initialization on the fly)
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        
+        // Pass 2: Pehla unique character dhundna
+        for (int i = 0; i < s.length(); i++) {
+            if (freq.get(s.charAt(i)) == 1) {
+                return i; 
+            }
+        }
+        
+        return -1;
     }
 }
