0a1a45fc19128f52d93609486d7bd58ffdefa936
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-07 10:58:17 +0530

    Time: 5 ms (98.16%) | Memory: 47.1 MB (24.40%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,40 +1,40 @@
 class Solution {
     public int firstUniqChar(String s) {
-         // 26 size ka array aur naam 'count' rakha (Java automatic sabko 0 kar dega)
-        int[] count = new int[26]; 
+        //  // 26 size ka array aur naam 'count' rakha (Java automatic sabko 0 kar dega)
+        // int[] count = new int[26]; 
         
-        // Pass 1: Frequency count update karna
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++; // Character se 'a' minus karke sahi dabba dhunda
-        }
-        
-        // Pass 2: Pehla unique character dhundna
-        for (int i = 0; i < s.length(); i++) {
-            if (count[s.charAt(i) - 'a'] == 1) {
-                return i; // Pehla 1 count wala character milte hi return
-            }
-        }
-        
-        return -1; // Agar koi unique nahi mila
-
-        // freq wala 
-
-        // Array ka naam 'freq' rakha, Java automatic sabko 0 kar dega
-        // int[] freq = new int[128]; 
-        
-        // // Pass 1: Frequency count karna
+        // // Pass 1: Frequency count update karna
         // for (int i = 0; i < s.length(); i++) {
-        //     freq[s.charAt(i)]++; 
+        //     count[s.charAt(i) - 'a']++; // Character se 'a' minus karke sahi dabba dhunda
         // }
         
         // // Pass 2: Pehla unique character dhundna
         // for (int i = 0; i < s.length(); i++) {
-        //     if (freq[s.charAt(i)] == 1) {
-        //         return i; 
+        //     if (count[s.charAt(i) - 'a'] == 1) {
+        //         return i; // Pehla 1 count wala character milte hi return
         //     }
         // }
         
-        // return -1;
+        // return -1; // Agar koi unique nahi mila
+
+        // freq wala 
+
+        // Array ka naam 'freq' rakha, Java automatic sabko 0 kar dega
+        int[] freq = new int[128]; 
+        
+        // Pass 1: Frequency count karna
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++; 
+        }
+        
+        // Pass 2: Pehla unique character dhundna
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i)] == 1) {
+                return i; 
+            }
+        }
+        
+        return -1;
 
         // // hashmap wala 
 
