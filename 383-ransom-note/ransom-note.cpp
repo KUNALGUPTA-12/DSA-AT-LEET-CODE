d935a7c9d0d616e11dc4b2cf86b71341867e7b10
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-07 12:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (49.41%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        // Vector hata kar asli unordered_map lagaya snake_case mein
-        unordered_map<char, int> magazine_freq; 
+        // // Vector hata kar asli unordered_map lagaya snake_case mein
+        // unordered_map<char, int> magazine_freq; 
         
-        // Loop 1: Dynamic map insertion
+        // // Loop 1: Dynamic map insertion
+        // for (int i = 0; i < magazine.length(); i++) {
+        //     char mag_char = magazine[i]; 
+        //     magazine_freq[mag_char]++; 
+        // }
+        
+        // // Loop 2: Checking from map stock
+        // for (int i = 0; i < ransomNote.length(); i++) {
+        //     char note_char = ransomNote[i]; 
+            
+        //     if (magazine_freq[note_char] == 0) {
+        //         return false; 
+        //     }
+        //     magazine_freq[note_char]--; 
+        // }
+        
+        // return true;
+        // vector se hua hai
+          // C++ snake_case variables setup
+        vector<int> magazine_freq(128, 0); 
+        
+        // Loop 1: Magazine ke characters ka stock bharna
         for (int i = 0; i < magazine.length(); i++) {
             char mag_char = magazine[i]; 
             magazine_freq[mag_char]++; 
         }
         
-        // Loop 2: Checking from map stock
+        // Loop 2: Ransom Note ke letters ko check aur minus karna
         for (int i = 0; i < ransomNote.length(); i++) {
             char note_char = ransomNote[i]; 
             
+            // Magazine wale vector stock (magazine_freq) se check kiya
             if (magazine_freq[note_char] == 0) {
                 return false; 
             }
