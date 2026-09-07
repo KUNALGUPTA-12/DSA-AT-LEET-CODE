178efc8c732a5d46cf5ca9f2586eec59d011bdf6
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-07 10:55:29 +0530

    Time: 2 ms (82.54%) | Memory: 14.5 MB (96.77%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -6,7 +6,7 @@ public:
         // second hashmap
         // third count ka 26
 
-        // vector<int> freq(128,0);
+        // std::vector<int> freq(128,0);
         
         // // pass 1
         // // for(int i = 0;i < s.length();i++){
@@ -25,17 +25,36 @@ public:
 
         // hasmap se 
 
-        std::unordered_map<char, int> freq;
+        // std::unordered_map<char, int> freq;
+        //  // pass 1
+        // // for(int i = 0;i < s.length();i++){
+        //     // char c = s[i];
+        // for(char c : s){
+        //     freq[c]++;
+        // }
+
+        // // pass two
+        // for(int i = 0;i < s.length();i++){
+        //     if(freq[s[i]] == 1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // count 
+
+        std::vector<int> count(26,0);
+
          // pass 1
         // for(int i = 0;i < s.length();i++){
             // char c = s[i];
         for(char c : s){
-            freq[c]++;
+            count[c - 'a']++;
         }
 
         // pass two
         for(int i = 0;i < s.length();i++){
-            if(freq[s[i]] == 1){
+            if(count[s[i] - 'a'] == 1){
                 return i;
             }
         }
