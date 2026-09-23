128dc7d3b61426ffe1139f4125e84786ecc1ddfc
KUNAL GUPTA <kunalgupta13.03.2006@gmail.com>
2026-09-23 16:26:26 +0530

    Time: 4 ms (74.25%) | Memory: 9.8 MB (69.83%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.cpp
@@ -0,0 +1,54 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+         int n = mountainArr.length();
+        
+        // Step 1: Find the Peak Element Index
+        int low = 0, high = n - 1;
+        int peak = 0;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        peak = low; // Pahad ki choti mil gayi
+        
+        // Step 2: Left side me normal Binary Search (Increasing Order)
+        low = 0;
+        high = peak;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid; // Pehla index mil gaya to return karo
+            else if (val < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        
+        // Step 3: Right side me Reverse Binary Search (Decreasing Order)
+        low = peak + 1;
+        high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            // Kyunki ye decreasing order hai, isliye logic ulta hoga
+            else if (val > target) low = mid + 1; // Bada hai to aage jao kyunki aage chote number hain
+            else high = mid - 1;
+        }
+        
+        return -1; // Agar pure pahad me target nahi mila
+    }
+};
